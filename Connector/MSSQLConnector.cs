f2e5437085cbbe478ae8a2c0963ce740beb9f983
Vai van Ommeren <vaivanommeren@gmail.com>
2017-01-06 14:28:25 +0100

    Begin gemaakt

---
+++ Connector/MSSQLConnector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace KillerAppSE2.Connector
+{
+    public class MSSQLConnector : IConnector
+    {
+        public MSSQLConnector()
+        {
+            dbCon =
+                ConfigurationManager.ConnectionStrings["KillerappSE2.Properties.Settings.Connectionstring"]
+                    .ConnectionString;
+        }
+
+        public string dbCon { get; }
+
+        public List<DataRow> ExecuteQueryCommand(SqlCommand command)
+        {
+            var con = new SqlConnection(dbCon);
+            command.Connection = con;
+
+            SqlDataAdapter dAtapter = new SqlDataAdapter(command);
+            DataTable dTable = new DataTable();
+            dAtapter.Fill(dTable);
+            var dList = dTable.AsEnumerable().ToList();
+
+            con.Close();
+
+            return dList;
+        }
+    }
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KillerAppSE2.Models
+{
+    public class User
+    {
+        public string name;
+        public int age;
+    }
+}
